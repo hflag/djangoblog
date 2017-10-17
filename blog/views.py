7605fa0ce336c816e9457a990aac0165397adef6
hflag <532843488@qq.com>
2017-10-17 19:19:35 +0800

    使用了类视图：PostListView，同时修改urls及pagination.html“

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render,get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from django.views.generic import ListView
 
 
 # Create your views here.
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    template_name = 'blog/post/list.html'
+    paginate_by = 2
+
+
 def post_list(request):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 2)
